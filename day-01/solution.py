446891b82446e6a70244322ca6e36af2add96ec3
Olly Middleton <olly@wegotpop.com>
2021-12-01 19:02:04 +0000

    adds solutions for part 1 and part 2

---
+++ day-01/solution.py
@@ -0,0 +1,34 @@
+def list_from_file(filename):
+    with open(filename) as file:
+        return [int(x) for x in file.read().split("\n")]
+
+
+def part_one(data):
+    count = 0
+    for i in range(1, len(data)):
+        if data[i] > data[i - 1]:
+            count += 1
+
+    return count
+
+
+def part_two(data):
+    count = 0
+    for i in range(1, len(data)):
+        if i > 2:
+            current = data[i] + data[i - 1] + data[i - 2]
+            previous = data[i - 1] + data[i - 2] + data[i - 3]
+            if current > previous:
+                count += 1
+    return count
+
+
+def main():
+    depthlist = list_from_file("input.txt")
+    print("Part 1: {}".format(part_one(depthlist)))
+
+    print("Part 2: {}".format(part_two(depthlist)))
+
+
+if __name__ == "__main__":
+    main()
