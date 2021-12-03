f05cc6a4d58e0b029d319b767f06e16f07560f5c
Olly Middleton <olly@wegotpop.com>
2021-12-03 11:33:43 +0000

    day 3 WIP

---
+++ day-03/solution.py
@@ -0,0 +1,17 @@
+def list_from_file(filename):
+    with open(filename) as file:
+        return file.read().splitlines()
+
+
+def part_one(binaries):
+
+    return int(gamma, 2) * int(epsilon, 2)
+
+
+def main():
+    bin_list = list_from_file("input.txt")
+    print(bin_list)
+
+
+if __name__ == "__main__":
+    main()
