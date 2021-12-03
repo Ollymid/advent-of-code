4b42c1f862952981dc6d0384fd4f9d94b023ecd1
Olly Middleton <olly@wegotpop.com>
2021-12-03 21:24:11 +0000

    part 3 part 2 with a little help

--- day-03/solution.py
+++ day-03/solution.py
@@ -2,27 +2,44 @@ from collections import Counter
 from typing import List
 
 
-def list_from_file(filename) -> List[str]:
-    with open(filename) as file:
-        return file.read().splitlines()
+def part_one() -> int:
+    with open("input.txt") as file:
+        cols = zip(*[[i for i in x.strip()] for x in file])
 
-
-def part_one(binaries):
-    cols = zip(*[[int(i) for i in x.strip()] for x in binaries])
-
-    gamma = "".join([str(Counter(c).most_common(1)[0][0]) for c in cols])
+    gamma = "".join([Counter(c).most_common(1)[0][0] for c in cols])
     epsilon = "".join("1" if char is "0" else "0" for char in gamma)
 
     return int(gamma, 2) * int(epsilon, 2)
 
 
-def part_two(binaries):
-    pass
+def part_two():
+    with open("input.txt") as file:
+        lines = [x.strip() for x in file]
+
+    o2 = lines
+    co2 = lines
+    i = 0
+    while len(o2) > 1 or len(co2) > 1:
+        common = max(
+            Counter(s[i] for s in o2).items(), key=lambda entry: (entry[1], entry[0])
+        )[0]
+        uncommon = min(
+            Counter(s[i] for s in co2).items(), key=lambda entry: (entry[1], entry[0])
+        )[0]
+
+        o2 = [s for s in o2 if s[i] == common]
+        co2 = [s for s in co2 if s[i] == uncommon]
+        i += 1
+
+    # common   '010111110111'
+    # uncommon '100111001110'
+
+    return int(o2[0], 2) * int(co2[0], 2)
 
 
 def main():
-    bin_list = list_from_file("input.txt")
-    print(f"Part 1: {part_one(bin_list)}")
+    print(f"Part 1: {part_one()}")
+    print(f"Part 2: {part_two()}")
 
 
 if __name__ == "__main__":
